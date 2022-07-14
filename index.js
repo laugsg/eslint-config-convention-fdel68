c566859899d045fb72987df5fd49baf8cedc709c
laugsg <lautaro.grucci@joingsg.com>
2022-07-14 15:37:01 +0200

    feat(FDEL-68): update > rules from external packages

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "import", "foldernaming"],
+  plugins: ["@typescript-eslint", "import", "filenames-simple", "react", "foldernaming"],
   rules: {
-    "import/no-default-export": "error"
-  }
+    "import/no-default-export": "error",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      { selector: "typeLike", format: ["PascalCase"] },
+    ],
+    "filenames-simple/naming-convention": ["error", { "rule": "kebab-case" }],
+    "foldernaming/match-regex": [2, "^[a-z-]+$", './packages/'],
+    "react/jsx-pascal-case": 2
+  },
 };
