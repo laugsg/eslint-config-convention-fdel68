ee63a22fe88c3ca196e72ac7f5358fec48fed24e
laugsg <lautaro.grucci@joingsg.com>
2022-07-19 14:04:45 +0200

    feat(FDEL-68): update > version

--- config/lint-staged.config.js
+++ config/lint-staged.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
-const fileSystemConfig = require("./convention-FDEL68-filesystem.config")
-
+const fileSystemConfig = require("./convention-FDEL68-filesystem.config");
 
 /** Convention FDEL-68
  * Filename should be one of the allowed variants
@@ -9,9 +8,7 @@ const fileSystemConfig = require("./convention-FDEL68-filesystem.config")
  * - .lintstagedrc (JS | JSON | YML | MJS | CJS)
  */
 
-module.exports = {
-  ...fileSystemConfig,
-  "*":(absolutePaths) => {
+module.exports = (absolutePaths) => {
   const cwd = process.cwd();
   const relativePaths = absolutePaths.map((file) => path.relative(cwd, file));
   const filteredPaths = relativePaths.filter((file) => {
@@ -20,7 +17,7 @@ module.exports = {
      * they are filtered out from paths to provide just allowed ones.
      */
     if (
-      ! file.includes(".config") &&
+      !file.includes(".config") &&
       path.extname(file) !== ".json" &&
       path.extname(file) !== ".md" &&
       path.extname(file) !== ""
@@ -28,6 +25,9 @@ module.exports = {
       return file;
     }
   });
-  return [`npx eslint -c ${process.cwd()}/node_modules/eslint-config-convention-fdel68/config/convention-FDEL68.naming-rules.config.json ${filteredPaths.join(" ")}`];
-}
-}
+  return [
+    `npx eslint -c convention-FDEL68.naming-rules.config.json ${filteredPaths.join(
+      " "
+    )}`,
+  ];
+};
