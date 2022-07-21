469d49d63f797e90ad889aabe3b2cb5a373ae2f2
laugsg <lautaro.grucci@joingsg.com>
2022-07-21 14:53:19 +0200

    feat(FDEL-68): test > refactor based in a store concept

--- config/convention-FDEL68-filesystem.config.js
+++ config/convention-FDEL68-filesystem.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
+//#region
 let response = [];
 
 /**
@@ -54,11 +55,14 @@ function checkFilesInComponentRoot(filename) {
       return false;
   }
 }
+//#endregion
 
 function validatefilesInRoot(relativePaths) {
 
     relativePaths.forEach((item) => {
+      // console.log("item",item)
         const query = item.split(path.sep);
+        // console.log("query",query)
         const splittedPath = query.slice(0, 3)
         if (query[0] === 'packages' && query.length >= 3) {
           if (fs.lstatSync(path.join(...splittedPath)).isFile()) {
@@ -69,7 +73,7 @@ function validatefilesInRoot(relativePaths) {
           }
           else if (fs.lstatSync(path.join(...splittedPath)).isDirectory()) {
             if (splittedPath[2] === 'src' || splittedPath[2] === 'dist') {
-              console.log("should be an index", item)
+              // console.log("should be an index", item)
               if (fs.lstatSync(path.join(...query.slice(0, 4))).isDirectory()) {
                       if (query[3] !== 'components' && query[3] !== 'utils'){
                           response = checkResponse(`Unexpected ${path.join(...query.slice(3, 4))}/ folder name under /src (allowed only /components and /utils)`)
@@ -85,8 +89,6 @@ function validatefilesInRoot(relativePaths) {
 
 
     });
-
-    // console.log("response", response)
     
     Object.keys(filesInRoot).map((filename) => {
       if (checkFileIsNotPresent(filename)){
@@ -106,9 +108,51 @@ function checkResponse(message) {
 }
 
 module.exports = {
-  "packages/**/*.{ts,tsx}":(absolutePaths) => {
+  "packages/**":(absolutePaths) => {
     const cwd = process.cwd();
     const relativePaths = absolutePaths.map((file) => path.relative(cwd, file));
+
+
+
+  /** Get directory : list
+   * Extract the component name from each of staged files
+   * to read the Component folder to validate its structure.
+   * @returns {array} example: ['aaa', 'bbb']
+   */
+  const componentPackages = [...new Set(relativePaths.map((item) => {
+    const query = item.split(path.sep);
+    if (
+      query[0] === 'packages' &&
+      fs.lstatSync(path.join(...query.slice(0, 2))).isDirectory()
+      ) {
+        return query[1]
+      }
+    }))]
+
+    /** Get directory tree
+     * Use the list of directories to read them
+     * and provide the directory tree
+     * @returns {array} example: ['index.ts', 'folder', 'file.js']
+     */
+    const componentTree = componentPackages.map(folder => {
+      // console.log("folder",folder)
+      const root = fs.readdirSync(path.resolve(__dirname, 'packages', folder))
+      // console.log("root", root)
+      return root.map(file => {
+        // console.log("folder[index]",folder[index])
+        // console.log("file",file)
+        const fullPath = path.resolve(__dirname, 'packages', folder, file)
+        const query = fullPath.split(path.sep);
+        // console.log("query",query)
+        const shortPath = query.splice(4, query.length - 1)
+        // console.log("shortPath",shortPath)
+        const normalizedPath = path.join(...shortPath)
+        // console.log("normalizedPath",normalizedPath)
+        return normalizedPath
+      })
+      // return root
+    })
+    console.log("componentTree",componentTree)
 
     return validatefilesInRoot(relativePaths);
     // return ['0']
